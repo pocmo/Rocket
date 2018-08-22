1c1ed387124a5a43a03ef111a03d39fbb232630c
cnevinc <--global>
2018-08-22 16:05:02 +0800

    ktlint has different continous indent with Android Studio. So I don't want to merge this PR

--- app/src/main/java/org/mozilla/rocket/privately/browse/BrowserFragment.kt
+++ app/src/main/java/org/mozilla/rocket/privately/browse/BrowserFragment.kt
@@ -62,9 +62,11 @@ class BrowserFragment : LocaleAwareFragment(),
 
     private var isLoading: Boolean = false
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_private_browser, container, false)
     }
